e5aaa9eb514ba3c866a16b18dda4d924176576e7
sunghyuckhong <sunghyuck.hong@furiosa.ai>
2024-02-08 09:16:31 +0000

    make dataloader for generation and graph with kv_cache

--- language/gpt-j/quantization/custom_symbolic_trace.py
+++ language/gpt-j/quantization/custom_symbolic_trace.py
@@ -1,38 +1,40 @@
 from transformers import PreTrainedModel, GPTJForCausalLM
-from transformers.utils.fx import HFTracer, get_concrete_args, check_if_model_is_supported 
+from transformers.utils.fx import HFTracer, get_concrete_args, check_if_model_is_supported
 import torch
 from torch.fx import Tracer, GraphModule
 from typing import Any, Callable, Dict, List, Optional, Type, Union
 
 
 def get_input_names_and_concrete_args(model: PreTrainedModel):
-    if type(model)==GPTJForCausalLM:
-        custom_concrete_args = {'use_cache' : False, 'return_dict' : True, 'output_attentions': False, 'output_hidden_states': False} 
-        input_names = ["input_ids", "position_ids", "attention_mask"]
-    else:    
+    if type(model) == GPTJForCausalLM:
+        custom_concrete_args = {'use_cache': True, 'return_dict': True,
+                                'output_attentions': False, 'output_hidden_states': False}
+        input_names = ["input_ids", "past_key_values",
+                       "position_ids", "attention_mask"]
+    else:
         raise NotImplementedError
-    
+
     if input_names is None:
         input_names = model.dummy_inputs.keys()
-        
+
     input_names = list(input_names)
-    concrete_args = get_concrete_args(model, input_names )
-    
+    concrete_args = get_concrete_args(model, input_names)
+
     for args in custom_concrete_args.keys():
         if args not in concrete_args.keys():
-            raise ValueError(f'{args} does not belong to {concrete_args}.' )
+            raise ValueError(f'{args} does not belong to {concrete_args}.')
         concrete_args[args] = custom_concrete_args[args]
-    
-    return input_names, concrete_args  
-    
+
+    return input_names, concrete_args
+
 
 def custom_symbolic_trace(model: PreTrainedModel,
-    input_names: Optional[List[str]] = None,
-    custom_concrete_args: Dict = None, 
-    disable_check: bool = False,
-    tracer_cls: Type[HFTracer] = HFTracer,
-) -> GraphModule:
-    
+                          input_names: Optional[List[str]] = None,
+                          custom_concrete_args: Dict = None,
+                          disable_check: bool = False,
+                          tracer_cls: Type[HFTracer] = HFTracer,
+                          ) -> GraphModule:
+
     input_names, concrete_args = get_input_names_and_concrete_args(model)
 
     if not disable_check:
@@ -50,4 +52,3 @@ def custom_symbolic_trace(model: PreTrainedModel,
     traced.device = model.device
 
     return traced, input_names, concrete_args
-    
--- language/gpt-j/quantization/get_quant_model.py
+++ language/gpt-j/quantization/get_quant_model.py
@@ -1,5 +1,5 @@
 import yaml
-import os 
+import os
 import torch
 from torch.utils.data import DataLoader
 from transformers.utils.fx import symbolic_trace
@@ -13,14 +13,26 @@ gen_kwargs = {
     "early_stopping": True,
     "max_new_tokens": 128,
     "min_new_tokens": 30,
-    "num_beams": int(os.environ.get("GPTJ_BEAM_SIZE", "4")), # only beam_size 4 is allowed for official submission
+    # only beam_size 4 is allowed for official submission
+    "num_beams": int(os.environ.get("GPTJ_BEAM_SIZE", "4")),
 }
 
-def make_dataloader(data_object, batch_size):
+
+def make_dataloader(data_object, batch_size, model_config, use_cache=False, gen_mode=False):
     data_list = []
     for idx in range(len(data_object.source_encoded_input_ids)):
-        data_list.append({'input_ids': data_object.source_encoded_input_ids[idx], 'attention_mask': data_object.source_encoded_attn_masks[idx], 'position_ids': torch.arange(len(data_object.source_encoded_input_ids[idx][0]))})
-    
+        if use_cache == False and gen_mode == False:
+            data_list.append({'input_ids': data_object.source_encoded_input_ids[idx], 'attention_mask': data_object.source_encoded_attn_masks[idx], 'position_ids': torch.arange(
+                len(data_object.source_encoded_input_ids[idx][0]))})
+        elif use_cache == True and gen_mode == True:
+            data_list.append({'input_ids': data_object.source_encoded_input_ids[idx][0, -1].reshape(1, 1), 'past_key_values': get_dummy_kv_cache(data_object.source_encoded_input_ids[idx], model_config), 'attention_mask': torch.ones(
+                len(data_object.source_encoded_input_ids[0][0])+1).unsqueeze(0).type(torch.int), 'position_ids': torch.tensor(len(data_object.source_encoded_input_ids[idx][0])).reshape(1, 1)})
+        elif use_cache == True and gen_mode == False:
+            data_list.append({'input_ids': data_object.source_encoded_input_ids[idx][0, -1].reshape(1, 1).repeat(gen_kwargs["num_beams"], 1), 'past_key_values': get_dummy_kv_cache(data_object.source_encoded_input_ids[idx], model_config), 'attention_mask': torch.ones(
+                len(data_object.source_encoded_input_ids[0][0])+1).unsqueeze(0).repeat(gen_kwargs["num_beams"], 1).type(torch.int), 'position_ids': torch.tensor(len(data_object.source_encoded_input_ids[idx][0])).reshape(1, 1).repeat(gen_kwargs["num_beams"], 1)})
+        elif use_cache == False and gen_mode == True:
+            raise ValueError(
+                "Not implemented yet. Will implement when need arises.")
     return DataLoader(data_list, batch_size)
 
 
@@ -30,20 +42,37 @@ def load_model_script(model_script_path):
 
     return model_script
 
-        
-    
+
+def get_dummy_kv_cache(input_ids, model_config):
+    kv_cache = list(range(model_config.n_layer))
+    for idx in range(len(kv_cache)):
+        kv_cache[idx] = [torch.randn(gen_kwargs["num_beams"], model_config.n_head, len(
+            input_ids[0]), int(model_config.n_embd/model_config.n_head)) for _ in range(2)]
+
+    return list(kv_cache)
+
+
+# def get_position_ids(num_tokens, device):
+
+#     position_ids = torch.zeros((gen_kwargs["num_beams"], num_tokens))
+#     for idx in range(len(position_ids)):
+#         position_ids[idx]=torch.arange(num_tokens)
+#     position_ids.to(device)
+
+#     return position_ids
 
 
 def get_quant_model(model, data_object, model_script_path):
-    #Load model script and calibration dataloader
+    # Load model script and calibration dataloader
     model_script = load_model_script(model_script_path)
-    calib_dataloader = make_dataloader(data_object, model_script['calib_batch_size'])
-    
-    #Extract necessary parameters to initialize QuantPreTrainedModel
+
+    calib_dataloader = make_dataloader(
+        data_object, model_script['calib_batch_size'], model.config, model.config.use_cache, gen_mode=False)
+    # model.generate(**calib_dataloader[0],**gen_kwargs)
+    # model(**calib_dataloader_forward[0])
+    # Extract necessary parameters to initialize QuantPreTrainedModel
     model_type = type(model)
 
-
-    
     model, input_names, concrete_args = custom_symbolic_trace(model)
     model = model_compressor.create_quantsim_model(
         model,
@@ -59,7 +88,7 @@ def get_quant_model(model, data_object, model_script_path):
         target_machine=model_script["target_machine"],
         dataloader=calib_dataloader,
     )
-    
+
     # model_compressor.calibrate(
     #         model=model,
     #         #model_name=model_name,
@@ -91,9 +120,7 @@ def get_quant_model(model, data_object, model_script_path):
         act_dtype=model_script["act_dtype"],
         act_nbits=model_script["act_nbits"],
     )
-    
-    
+
     model.recompile()
-    
+
     return QuantPreTrainedModel(model, model_type, input_names, concrete_args)
- 
