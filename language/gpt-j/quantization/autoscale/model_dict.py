f33ced46d85262fed2e7fb54160859feac109226
sunghyuckhong <sunghyuck.hong@furiosa.ai>
2024-04-01 01:10:06 +0000

    add model_dict.py

---
+++ language/gpt-j/quantization/autoscale/model_dict.py
@@ -0,0 +1,12 @@
+import transformers
+import furiosa_llm_models
+
+
+#To DO: Add dictionaries for other models in furiosa-llm-models
+
+GPTJForCausalLM_dict = {
+    transformers.models.gptj.modeling_gptj.GPTJForCausalLM : transformers.models.gptj.modeling_gptj,
+    furiosa_llm_models.gptj.huggingface.GPTJForCausalLM : furiosa_llm_models.gptj.huggingface,
+    furiosa_llm_models.gptj.paged_attention_concat.GPTJForCausalLM : furiosa_llm_models.gptj.paged_attention_concat,
+    furiosa_llm_models.gptj.huggingface_rope.GPTJForCausalLM: furiosa_llm_models.gptj.huggingface_rope
+}
