13489135d314f7d4c2f23f8dec6fd32dae84bcce
jh619lee <jihye.lee@furiosa.ai>
2024-04-02 14:21:32 +0000

    added concat_rope

--- language/gpt-j/quantization/autoscale/model_dict.py
+++ language/gpt-j/quantization/autoscale/model_dict.py
@@ -8,5 +8,6 @@ GPTJForCausalLM_dict = {
     transformers.models.gptj.modeling_gptj.GPTJForCausalLM : transformers.models.gptj.modeling_gptj,
     furiosa_llm_models.gptj.huggingface.GPTJForCausalLM : furiosa_llm_models.gptj.huggingface,
     furiosa_llm_models.gptj.paged_attention_concat.GPTJForCausalLM : furiosa_llm_models.gptj.paged_attention_concat,
-    furiosa_llm_models.gptj.huggingface_rope.GPTJForCausalLM: furiosa_llm_models.gptj.huggingface_rope
+    furiosa_llm_models.gptj.huggingface_rope.GPTJForCausalLM: furiosa_llm_models.gptj.huggingface_rope,
+    furiosa_llm_models.gptj.paged_attention_concat_rope.GPTJForCausalLM: furiosa_llm_models.gptj.paged_attention_concat_rope
 }
