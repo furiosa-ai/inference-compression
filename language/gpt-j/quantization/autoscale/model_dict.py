18ff98eea48847443a9d79bd7059d25bedcc6d83
sunghyuckhong <sunghyuck.hong@furiosa.ai>
2024-04-29 05:34:23 +0000

    added unintended changes

--- language/gpt-j/quantization/autoscale/model_dict.py
+++ language/gpt-j/quantization/autoscale/model_dict.py
@@ -7,7 +7,6 @@ import furiosa_llm_models
 GPTJForCausalLM_dict = {
     transformers.models.gptj.modeling_gptj.GPTJForCausalLM : transformers.models.gptj.modeling_gptj,
     furiosa_llm_models.gptj.huggingface.GPTJForCausalLM : furiosa_llm_models.gptj.huggingface,
-    furiosa_llm_models.gptj.paged_attention_concat.GPTJForCausalLM : furiosa_llm_models.gptj.paged_attention_concat,
     furiosa_llm_models.gptj.huggingface_rope.GPTJForCausalLM: furiosa_llm_models.gptj.huggingface_rope,
     furiosa_llm_models.gptj.paged_attention_concat_rope.GPTJForCausalLM: furiosa_llm_models.gptj.paged_attention_concat_rope,
     furiosa_llm_models.gptj.preallocated_concat_rope.GPTJForCausalLM: furiosa_llm_models.gptj.preallocated_concat_rope,
