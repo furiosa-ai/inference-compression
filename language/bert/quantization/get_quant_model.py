656b087528432b388105dad32d8d8016e1f0e0da
jh619lee <jihye.lee@furiosa.ai>
2024-06-21 08:45:19 +0000

    update get_quant_model for compact_mask version

--- language/bert/quantization/get_quant_model.py
+++ language/bert/quantization/get_quant_model.py
@@ -39,12 +39,15 @@ def get_quant_model(sut, model_source, model_script_path, n_calib, recalibrate, 
         from .calib_dataloader import load_bert_calibration_data
         calib_eval_features = load_bert_calibration_data(sut.qsl, n_calib)
         
-        if model_source in ['mlperf_submission', "experimental_huggingface_unsplit_packed"]:
+        if model_source == 'mlperf_submission':
             from .calib_dataloader import make_packed_calib_data_loader
             if use_packed_dataloader:
                 calib_dataloader = make_packed_calib_data_loader(calib_eval_features, model_script['calib_batch_size'], n_calib, pad_token_id=0, bucket_size=384, compact_mask=False) 
             else:
                 calib_dataloader = make_dataloader(calib_eval_features, model_script['calib_batch_size'], n_calib, include_position_ids=True)
+        elif model_source == 'experimental_huggingface_unsplit_packed':
+            from .calib_dataloader import make_packed_calib_data_loader
+            calib_dataloader = make_packed_calib_data_loader(calib_eval_features, model_script['calib_batch_size'], n_calib, pad_token_id=0, bucket_size=384, compact_mask=True)                 
         else:            
             calib_dataloader = make_dataloader(calib_eval_features, model_script['calib_batch_size'], n_calib)
            
@@ -135,6 +138,9 @@ def get_quant_model(sut, model_source, model_script_path, n_calib, recalibrate, 
     if model_source == 'mlperf_submission':
         from furiosa_llm_models.generators.bert_generator import BertUnsplitPackedGenerator
         generator = BertUnsplitPackedGenerator(model=quant_model, compact_mask=False)
+    elif model_source == 'experimental_huggingface_unsplit_packed':
+        from furiosa_llm_models.generators.bert_generator import BertUnsplitPackedGenerator
+        generator = BertUnsplitPackedGenerator(model=quant_model, compact_mask=True)   
     else:
         generator = None
     
